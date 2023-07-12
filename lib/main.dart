c07cc272c553bd9ccc6e61d8b3d7b2e707849a79
chenjun <chenjun@orvibo.com>
2023-07-12 16:51:34 +0800

    add cj_kit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,25 @@
+import 'package:cj_kit/cj_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_advanced/pages/main_page.dart';
 import 'package:flutter_advanced/pages/not_found_page.dart';
 import 'package:flutter_advanced/routes/routes.dart';
 import 'package:flutter_advanced/store/root_store.dart';
 
 void main() {
-  runApp(const MyApp());
+  CjKit.runApp(
+    app: const MyApp(),
+    preRun: () async {
+      WidgetsFlutterBinding.ensureInitialized();
+      await SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: <SystemUiOverlay>[]);
+      // 调整Flutter中图片缓存的最大大小,图片缓存的最大数量为200张图,图片缓存的最大字节大小为40MB。
+      PaintingBinding.instance.imageCache.maximumSize = 200;
+      PaintingBinding.instance.imageCache.maximumSizeBytes = 40 << 20;
+    },
+    logCallback: (String line){
+
+    }
+  );
 }
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
