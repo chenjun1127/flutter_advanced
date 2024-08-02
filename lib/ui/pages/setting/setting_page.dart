48a641e4435505fb57b23e05afde97393ac9ce32
chenjun <chenjun@orvibo.com>
2024-08-02 19:04:00 +0800

    +: 修复警告

--- lib/ui/pages/setting/setting_page.dart
+++ lib/ui/pages/setting/setting_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_advanced/routes/animate_route.dart';
-
-import '../../../routes/scale_page_route.dart';
+import 'package:flutter_advanced/routes/scale_page_route.dart';
 
 class SettingPage extends StatefulWidget {
   const SettingPage({Key? key}) : super(key: key);
@@ -25,10 +24,12 @@ class _SettingPageState extends State<SettingPage> {
     );
   }
 
-  void toPge(){
-    scalePageRoute(builder: (BuildContext context) {
-      return SecondPage();
-    }, context: context);
+  void toPge() {
+    scalePageRoute(
+        builder: (BuildContext context) {
+          return const SecondPage();
+        },
+        context: context);
     // Navigator.of(context).push(createRoute1(SecondPage()));
     // Navigator.of(context).push(_createRoute());
   }
@@ -44,7 +45,6 @@ class _SettingPageState extends State<SettingPage> {
     );
   }
 }
-
 
 class SecondPage extends StatelessWidget {
   const SecondPage({super.key});
@@ -69,146 +69,6 @@ class SecondPage extends StatelessWidget {
             Text("${ModalRoute.of(context)!.settings.arguments}")
           ],
         ),
-      ),
-    );
-  }
-}
-
-
-Route _createRoute() {
-  return PageRouteBuilder(
-    pageBuilder: (context, animation, secondaryAnimation) => SecondPage(),
-    transitionsBuilder: (context, animation, secondaryAnimation, child) {
-      return _ZoomPageTransition(
-        animation: animation,
-        secondaryAnimation: secondaryAnimation,
-        isScale: true,
-        child: child,
-      );
-    },
-    transitionDuration: const Duration(milliseconds: 300),
-    reverseTransitionDuration: const Duration(milliseconds: 300),
-  );
-}
-
-class _ZoomPageTransition extends StatelessWidget {
-  const _ZoomPageTransition({
-    required this.animation,
-    required this.secondaryAnimation,
-    this.isScale,
-    this.child,
-  });
-
-  final Animation<double> animation;
-  final Animation<double> secondaryAnimation;
-  final bool? isScale;
-  final Widget? child;
-
-  @override
-  Widget build(BuildContext context) {
-    return DualTransitionBuilder(
-      animation: animation,
-      forwardBuilder: (
-          BuildContext context,
-          Animation<double> animation,
-          Widget? child,
-          ) {
-        return _ZoomEnterTransition(
-          animation: animation,
-          child: child,
-        );
-      },
-      reverseBuilder: (
-          BuildContext context,
-          Animation<double> animation,
-          Widget? child,
-          ) {
-        return _ZoomExitTransition(
-          child: child,
-          animation: animation,
-          reverse: true,
-        );
-      },
-      child: child,
-    );
-  }
-}
-
-class _ZoomEnterTransition extends StatelessWidget {
-  const _ZoomEnterTransition({
-    required this.animation,
-    required this.child,
-    this.isScale = false,
-    this.reverse = false,
-  });
-
-  final Animation<double> animation;
-  final Widget? child;
-  final bool isScale;
-  final bool reverse;
-
-  @override
-  Widget build(BuildContext context) {
-    final scaleAnimation = Tween<double>(
-      begin: reverse ? 1.0 : 0.0,
-      end: 1.0,
-    ).animate(CurvedAnimation(
-      parent: animation,
-      curve: Curves.easeInOut,
-    ));
-
-    final fadeAnimation = Tween<double>(
-      begin: reverse ? 1.0 : 0.0,
-      end: 1.0,
-    ).animate(CurvedAnimation(
-      parent: animation,
-      curve: Curves.easeInOut,
-    ));
-
-    return FadeTransition(
-      opacity: fadeAnimation,
-      child: ScaleTransition(
-        scale: isScale ? scaleAnimation : animation,
-        child: child,
-      ),
-    );
-  }
-}
-
-class _ZoomExitTransition extends StatelessWidget {
-  const _ZoomExitTransition({
-    required this.animation,
-    required this.child,
-    this.reverse = false,
-  });
-
-  final Animation<double> animation;
-  final Widget? child;
-  final bool reverse;
-
-  @override
-  Widget build(BuildContext context) {
-    final scaleAnimation = Tween<double>(
-      begin: 1.0,
-      end: reverse ? 1.0 : 0.0,
-    ).animate(CurvedAnimation(
-      parent: animation,
-      curve: Curves.easeInOut,
-    ));
-
-    final fadeAnimation = Tween<double>(
-      begin: 1.0,
-      end: reverse ? 1.0 : 0.0,
-    ).animate(CurvedAnimation(
-      parent: animation,
-      curve: Curves.easeInOut,
-    ));
-
-    return FadeTransition(
-      opacity: fadeAnimation,
-      child: ScaleTransition(
-        scale: scaleAnimation,
-        child: child,
       ),
     );
   }
