0fdec8126b2e241a91bd93c6b4b6152a688c22e6
chenjun <chenjun@orvibo.com>
2024-08-15 11:31:15 +0800

    *: 设置 macos desktop窗口的大小

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -3,11 +3,28 @@ import FlutterMacOS
 
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
+    // 创建 Flutter 视图控制器
     let flutterViewController = FlutterViewController.init()
+
+    // 获取当前窗口的框架（尺寸和位置）
     let windowFrame = self.frame
+
+    // 设置窗口的内容视图控制器为 Flutter 视图控制器
     self.contentViewController = flutterViewController
+
+    // 设置窗口的框架（尺寸和位置）
     self.setFrame(windowFrame, display: true)
 
+    // 设置窗口的内容大小
+    self.setContentSize(NSSize(width: 1024, height: 600))
+
+    // 设置窗口背景颜色
+    self.backgroundColor = NSColor.black
+
+    // 移除窗口的可调整大小功能
+    self.contentView?.window?.styleMask.remove(.resizable)
+
+    // 注册插件
     RegisterGeneratedPlugins(registry: flutterViewController)
 
     super.awakeFromNib()
